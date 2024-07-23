29ff79e6fdab06c9bdf9aa35d8a013f6e1488849
Misaiasx <mizaiascorrea@gmail.com>
2024-07-23 09:06:38 -0300

    DevClubTesla

---
+++ script.js
@@ -0,0 +1,9 @@
+let fromulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+  fromulario.style.left = "30%"
+}
+
+function desaparecerFormulario(){
+  formulario.style.left = "-50%"
+}
